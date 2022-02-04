ef97e8136890df93202e7fa33abb46e25ed0df5d
sventura102 <sventura102@gmail.com>
2022-02-03 22:23:52 -0800

    yes

---
+++ lesson5/js/scripts.js
@@ -0,0 +1,7 @@
+const list = document.querySelector('list');
+const input = document.querySelector('input');
+const button = document.querySelector('button');
+
+button.addEventListener('click', function() {
+    Element.remove();
+}
