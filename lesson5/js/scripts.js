490cbac49aa77b5504c7cbeb29f8168421da5dff
sventura102 <sventura102@gmail.com>
2022-02-04 06:10:44 -0800

    yes

--- lesson5/js/scripts.js
+++ lesson5/js/scripts.js
@@ -1,7 +1,24 @@
-const list = document.querySelector('list');
-const input = document.querySelector('input');
-const button = document.querySelector('button');
+const list = document.querySelector('ul');
+      const input = document.querySelector('input');
+      const button = document.querySelector('button');
 
-button.addEventListener('click', function() {
-    Element.remove();
-}
+      button.onclick = function() {
+        let myItem = input.value;
+        input.value = '';
+
+        const listItem = document.createElement('li');
+        const listText = document.createElement('span');
+        const listBtn = document.createElement('button');
+
+        listItem.appendChild(listText);
+        listText.textContent = myItem;
+        listItem.appendChild(listBtn);
+        listBtn.textContent = '❌';
+        list.appendChild(listItem);
+
+        listBtn.onclick = function(e) {
+          list.removeChild(listItem);
+        }
+
+        input.focus();
+      }
