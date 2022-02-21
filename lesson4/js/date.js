5970cb06f664358f27c19eb05dbe33dd3b55a654
sventura102 <sventura102@gmail.com>
2022-02-20 20:51:10 -0800

    yes

--- lesson4/js/date.js
+++ lesson4/js/date.js
@@ -1,5 +1,5 @@
 // select the elements to manipulate (output to)
-const datefield = document.querySelector("date");
+const datefield = document.querySelector("#date");
 // derive the current date using a date object
 const now = new Date();
 const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
