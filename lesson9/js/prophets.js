d51b589da9c58cf2451a05a6261cdd137c5b5637
sventura102 <sventura102@gmail.com>
2022-03-12 19:21:10 -0800

    ...

--- lesson9/js/prophets.js
+++ lesson9/js/prophets.js
@@ -6,22 +6,23 @@ fetch(requestURL)
     return response.json();
   })
   .then(function (jsonObject) {
-    console.table(jsonObject);  
-    prophets.forEach(displayProphets);// temporary checking for valid response and data parsing
-  });
-
-  const prophets = jsonObject['prophets'];
+    const prophets = jsonObject['prophets'];
+    prophets.forEach(displayProphets);
+    // temporary checking for valid response and data parsing
+  });  
 
   function displayProphets(prophet) {
     // Create elements to add to the document
   let card = document.createElement('section');
   let h2 = document.createElement('h2');
   let portrait = document.createElement('img');
+  let p1 = document.createElement('p');
+  let p2 = document.createElement('p');
 
   // Change the textContent property of the h2 element to contain the prophet's full name
   h2.textContent = `${prophet.name} ${prophet.lastname}`;
-  h2.textContent = `Date of Birth: ${birthdate}`;
-  h2.textContent = ``;
+  p1.textContent = `Date of Birth: ${prophet.birthdate}`;
+  p2.textContent = `Place of Birth: ${prophet.birthplace}`;
 
   // Build the image attributes by using the setAttribute method for the src, alt, and loading attribute values. (Fill in the blank with the appropriate variable).
   portrait.setAttribute('src', prophet.imageurl);
@@ -30,6 +31,8 @@ fetch(requestURL)
 
   // Add/append the section(card) with the h2 element
   card.appendChild(h2);
+  card.appendChild(p1);
+  card.appendChild(p2);
   card.appendChild(portrait);
 
   // Add/append the existing HTML div with the cards class with the section(card)
