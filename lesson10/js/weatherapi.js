b97008f2e03e20bbe5258107284c585546b354ea
sventura102 <sventura102@gmail.com>
2022-03-22 22:07:36 -0700

    hello world

---
+++ lesson10/js/weatherapi.js
@@ -0,0 +1,19 @@
+const apiURL = 'https://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&appid=6d49d5df8d56fd9a810eaf835830927f';
+fetch(apiURL)
+    .then((response) => response.json())
+    .then((jsObject) => {
+        console.log(jsObject);
+
+        const currentTemp = document.querySelector('#current-temp');
+        const weathericon = document.querySelector('#weathericon');
+
+        currentTemp.textContent = jsObject.main.temp.toFixed(0);
+        const imgsrc = `https://openweathermap.org/img/w/${jsObject.weather[0].icon}.png`
+        const imgalt = jsObject.weather[0].description;
+
+        currentTemp.textContent = jsObject.main.temp.toFixed(0);
+        weathericon.setAttribute('src', imgsrc);
+        weathericon.setAttribute('alt', imgalt);
+    });
+
+    
